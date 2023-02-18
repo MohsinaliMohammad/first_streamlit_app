6048c9d444a257decd726ffbbfe491345214cd12
Mohsin Ali <71037378+MohsinaliMohammad@users.noreply.github.com>
2023-02-18 23:46:30 +0530

    Update streamlit_app.py
    Call the Fruityvice API from Our Streamlit App

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -19,3 +17,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.text('Fruitvice Fruit Advice!')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
